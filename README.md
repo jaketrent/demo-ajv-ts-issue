b05c6a62ccfecd2b7da5554ab26e3f42cd2fd3a8
Jake Trent <jake.trent@angel.com>
2024-04-04 17:41:08 -0600

    better for uncommenting

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Demo to show current challenge with additionalProperties in JTD types for ajv.
 
+Related to [ajv-validator/ajv#2405](https://github.com/ajv-validator/ajv/issues/2405).
+
 ## To run
 
 ```
 npm install
-# uncomment desired test case
+
+# uncomment desired test case in src/main.ts, then:
+
 npm test
 ```
--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,39 @@ const ajv = new Ajv();
 // uncomment each case to try it out in turn
 console.log("Uncomment a test case in src/main.ts");
 
-//
-// Case #1 - define my own TS interface
-// tsc works, keeps me from 2 coding errors
+// // -------------------
+// //
+// // Case #1 - define my own TS interface
+// // tsc works, keeps me from 2 coding errors
 // interface Person {
 //   name: string;
 // }
 
 // const p1: Person = { name: "Alice", age: 32 };
-
-// tsc err:
-// src/main.ts:9:37 - error TS2353: Object literal may only specify known properties, and 'age' does not exist in type 'Person'.
-// 9 const p1: Person = { name: "Alice", age: 32 };
+// // tsc err:
+// // src/main.ts:9:37 - error TS2353: Object literal may only specify known properties, and 'age' does not exist in type 'Person'.
+// // 9 const p1: Person = { name: "Alice", age: 32 };
 
 // console.log("Does a person have an age?", p1.age);
-// src/main.ts:27:46 - error TS2339: Property 'age' does not exist on type 'Person'.
-// 27 console.log("Does a person have an age?", p1.age);
+// // tsc err:
+// // src/main.ts:27:46 - error TS2339: Property 'age' does not exist on type 'Person'.
+// // 27 console.log("Does a person have an age?", p1.age);
 
-//
-// Case #2 - assign unknown json to a TS interface, as in a network request
-// tsc doesn't know any better at build time. No type error, no help.
+// // -------------------
+// //
+// // Case #2 - assign unknown json to a TS interface, as in a network request
+// // tsc doesn't know any better at build time. No type error, no help.
 // interface Person {
 //   name: string;
 // }
 
 // const p1: Person = { name: "Alice", age: 32 } as any;
 
-//
-// Case #3 - runtime validation with ajv, still my own TS interface
-// Hence the need for runtime data validation
-// And why we'd want to use additionalProperties: true
+// // -------------------
+// //
+// // Case #3 - runtime validation with ajv, still my own TS interface
+// // Hence the need for runtime data validation
+// // And why we'd want to use additionalProperties: true
 // interface Person {
 //   name: string;
 // }
@@ -55,9 +58,10 @@ console.log("Uncomment a test case in src/main.ts");
 //   console.log("Person is invalid", validate.errors);
 // }
 
-//
-// Case #4 - runtime validation, use ajv-specified TS interface
-// Where the build-time errors from Case #1 are not caught any more
+// // -------------------
+// //
+// // Case #4 - runtime validation, use ajv-specified TS interface
+// // Where the build-time errors from Case #1 are not caught any more
 
 // const personSchema = {
 //   additionalProperties: true,
@@ -82,9 +86,10 @@ console.log("Uncomment a test case in src/main.ts");
 // // no 'property does not exist on type' error
 // console.log("Does a person have an age?", p2.age);
 
-//
-// Case #5 - same as #4, but additionalProperties: false
-// Not valid any more, but good to have our tsc errors back
+// // -------------------
+// //
+// // Case #5 - same as #4, but additionalProperties: false
+// // Not valid any more, but good to have our tsc errors back
 
 // const personSchema = {
 //   additionalProperties: false,
@@ -104,7 +109,7 @@ console.log("Uncomment a test case in src/main.ts");
 // }
 
 // // error is back
-// // const p2: Person = { name: "Alice", age: 32 };
+// const p2: Person = { name: "Alice", age: 32 };
 
 // // error is back
-// // console.log("Does a person have an age?", p2.age);
+//  console.log("Does a person have an age?", p2.age);
